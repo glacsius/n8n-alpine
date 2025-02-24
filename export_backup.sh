aa0ae4c4c83061f2a9aade2618c711078ae3f0b2
Glaucio <glacsius@gmail.com>
2025-02-24 10:38:33 -0500

    fix: update output file names in export_backup script

--- export_backup.sh
+++ export_backup.sh
@@ -1,3 +1,3 @@
-n8n export:workflow --all --output=/home/file.json
-n8n export:credentials --all --output=/home/file.json
+n8n export:workflow --all --output=/home/n8n_workflow.json
+n8n export:credentials --all --output=/home/n8n_credentials.json
 echo "Exportação concluída! Arquivos gerados em /home/"
